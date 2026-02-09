5fd74fe2e996b3422461bde37e0c585b97c88c3e
danielax0 <131830215+danielax0@users.noreply.github.com>
2026-02-09 17:47:50 -0500

    Update video source path to public directory

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -47,7 +47,7 @@ const Index = () => {
             controls
             poster=""
           >
-            <source src="/intro.mp4" type="video/mp4" />
+            <source src="public/intro.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
